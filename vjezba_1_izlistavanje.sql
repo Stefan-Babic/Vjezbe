fbfb8f2ba49b4493876b5fd94cd86b11e2f4bd0a
Stefan Babić <stefan.babic.vu@gmail.com>
2021-05-22 20:31:44 +0200

    Izlistavanje

---
+++ vjezba_1_izlistavanje.sql
@@ -0,0 +1,11 @@
+use vjezba_1;
+
+select * from zena;
+
+select kratkamajica * from zena where hlace='%ana%'
+
+select * from svekar;
+
+select a.dukserica, f.asocijalno, e.hlace from svekar a inner join sestra_svekar b on a.sifra=b.svekar
+inner join sestra c on b.sestra=c.sifra inner join zena d on c.sifra=d.sestra inner join muskarac e on d.sifra=e.zena
+inner join mladic f on e.sifra=f.muskarac where d.hlace='%a' and c.haljina='%ba%' order by e.hlace desc;
