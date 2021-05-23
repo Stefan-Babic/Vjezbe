bb1cb45339cc42ddb73b3fb439f8ca3405de28b8
Stefan Babić <stefan.babic.vu@gmail.com>
2021-05-23 17:26:32 +0200

    Korekcija "=" u "like"

--- vjezba_1_izlistavanje.sql
+++ vjezba_1_izlistavanje.sql
@@ -8,4 +8,4 @@ select * from svekar;
 
 select a.dukserica, f.asocijalno, e.hlace from svekar a inner join sestra_svekar b on a.sifra=b.svekar
 inner join sestra c on b.sestra=c.sifra inner join zena d on c.sifra=d.sestra inner join muskarac e on d.sifra=e.zena
-inner join mladic f on e.sifra=f.muskarac where d.hlace='%a' and c.haljina='%ba%' order by e.hlace desc;
+inner join mladic f on e.sifra=f.muskarac where d.hlace='%a' and c.haljina like'%ba%' order by e.hlace desc;
