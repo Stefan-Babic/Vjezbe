5645cb3814374f4ceefe25a72286e490d56e28f2
Stefan Babić <stefan.babic.vu@gmail.com>
2021-05-24 03:13:30 +0200

    Unos, ažuriranje i brisanje vrijednosti

--- vjezba_11.sql
+++ vjezba_11.sql
@@ -70,3 +70,18 @@ alter table svekrva add foreign key(punac) references punac(sifra);
 alter table punac_mladic add foreign key(punac) references punac(sifra);
 alter table punac_mladic add foreign key(mladic) references mladic(sifra);
 
+insert into punac(kuna,vesta) values (2345.22,'Siva'),(324.22,'Crvena'),(324235.55,'Plava');
+insert into mladic(ogrlica,drugiputa,hlace) values (22,'2022-10-11','Crne'),(45,'2021-12-10','Sive'),(67,'2021-06-05','Smeđe');
+insert into punac_mladic(punac,mladic) values (1,2),(2,1),(3,3);
+insert into svekrva(narukvica,carape,punac) values (32,'Probušene',1),(45,'Čitave',2),(67,'Oprane',3);
+insert into djevojka(kratkamajica,ekstrovertno,majica,introvertno) values ('Plava',1,'Plava',0),('Zelena',0,'Zelena',0),('Crvena',1,'Crvena',0);
+insert into neprijatelj(narukvica,novcica,bojakose,introvertno) values (45,3354.44,'Plava',0),(12,456.44,'Crna',0),(68,1234.11,'Smeđa',1);
+insert into muskarac(narukvica,neprijatelj) values (32,1),(45,2),(56,3);
+insert into punica(carape,majica,haljina,bojakose,djevojka) values('Iznošene','Plava','Roza','Plava',1),('Poderane','Žuta','Crvena','Crna',2),('Nove','Bijela','Ljubičasta','Crvena',3);
+
+update muskarac set novcica=15.70;
+select * from muskarac;
+
+delete from punica where drugiputa='2019-04-08';
+
+
